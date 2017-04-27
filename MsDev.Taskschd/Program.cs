c3ce818d5ba7edbb5f0b75b22607201af51e9ec7
NilTor <zpty@outlook.com>
2017-04-27 21:31:52 +0800

    simple update

--- MsDev.Taskschd/Program.cs
+++ MsDev.Taskschd/Program.cs
@@ -13,52 +13,63 @@ using System.IO;
 
 namespace MSDev.Taskschd
 {
-    public static class Program
+  public static class Program
+  {
+    private static IServiceCollection services = new ServiceCollection();
+
+    public static void Main(string[] args)
     {
-        private static IServiceCollection services = new ServiceCollection();
+      Console.OutputEncoding = Encoding.UTF8;
+      Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-        public static void Main(string[] args)
-        {
-            Console.OutputEncoding = Encoding.UTF8;
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+      // 加载配置文件
+      var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("config.json");
 
-            // 加载配置文件
-            var builder = new ConfigurationBuilder()
-                  .SetBasePath(Directory.GetCurrentDirectory())
-                  .AddJsonFile("config.json");
+      var config = builder.Build();
 
-            var config = builder.Build();
+      services.AddLogging();
 
+      ILoggerFactory factory = new LoggerFactory();
+      factory.AddConsole();
 
+      services.AddSingleton(factory);
 
-            services.AddLogging();
+      //连接字符串使用config.json中自定义的替换
+      services.AddDbContext<AppDbContext>(options =>
+          options.UseSqlServer(config.GetConnectionString("localSqlServer")));
 
-            ILoggerFactory factory = new LoggerFactory();
-            factory.AddConsole();
+      services.AddScoped<IBingNewsRepository, BingNewsAgent>();
+      services.AddScoped<BingNewsTask>();
 
-            services.AddSingleton(factory);
+      ////确保数据库建立
+      //var dbContext = new AppDbContext();
+      //dbContext.Database.Migrate();
 
-            //连接字符串使用config.json中自定义的替换
-            services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(config.GetConnectionString("localSqlServer")));
-
-            services.AddScoped<IBingNewsRepository, BingNewsAgent>();
-            services.AddScoped<BingNewsTask>();
-
-            ////确保数据库建立
-            //var dbContext = new AppDbContext();
-            //dbContext.Database.Migrate();
-
-            //Console.ReadLine();
-            Run();
-            Thread.Sleep(1000 * 10);
-        }
-
-        public static async void Run()
-        {
-            var task = services.BuildServiceProvider().GetService<BingNewsTask>();
-            await task.GetNews("微软");
-            Console.WriteLine("Done");
-        }
+      //Console.ReadLine();
+      Run("bingnews");
+      Thread.Sleep(1000 * 10);
     }
+
+    /// <summary>
+    /// 运行服务
+    /// </summary>
+    /// <param name="serviceName">服务名称</param>
+    public static async void Run(string serviceName)
+    {
+      serviceName = serviceName.ToLower();
+      switch (serviceName)
+      {
+        case "bingnews":
+          var task = services.BuildServiceProvider().GetService<BingNewsTask>();
+          await task.GetNews("微软");
+          Console.WriteLine("Done");
+          break;
+        default:
+          break;
+      }
+
+    }
+  }
 }
