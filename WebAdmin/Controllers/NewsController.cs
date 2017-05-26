97edbc6593ee6612fd040479832c03896b4a5d5b
NilTor <zpty@outlook.com>
2017-05-27 01:17:26 +0800

    daily upadte; edit News page

--- WebAdmin/Controllers/NewsController.cs
+++ WebAdmin/Controllers/NewsController.cs
@@ -1,20 +1,37 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MSDev.Task.Helpers;
+using MSDev.Task.Models;
+using Newtonsoft.Json;
 
 namespace WebAdmin.Controllers
 {
+	public class NewsController : BaseController
+	{
+		readonly ApiHelper _apiHelper;
+		public NewsController(ApiHelper apiHelper)
+		{
+			_apiHelper = apiHelper;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
 
+		[HttpGet]
+		public async Task<IActionResult> BingNews()
+		{
+			JsonResult<List<BingNews>> re = await _apiHelper.Get<List<BingNews>>("/api/manage/bingnews/pagelist");
+			if (re.ErrorCode == 0)
+			{
+				ViewBag.ListData = re.Data;
+				ViewBag.Page = re.PageOption;
 
-    public class NewsController : BaseController
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-
-    }
+				Console.WriteLine(JsonConvert.SerializeObject(re.PageOption));
+			}
+			return View();
+		}
+	}
 }
