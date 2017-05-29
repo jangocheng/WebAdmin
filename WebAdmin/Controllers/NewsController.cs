f33b1957763f1ff17bbff81651b7e2b26a8e14fe
NilTor <zpty@outlook.com>
2017-05-29 13:27:44 +0800

    edit error page

--- WebAdmin/Controllers/NewsController.cs
+++ WebAdmin/Controllers/NewsController.cs
@@ -34,5 +34,13 @@ namespace WebAdmin.Controllers
 			}
 			return View();
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> DelNews(string id)
+		{
+			JsonResult<string> re = await _apiHelper.Delete<string>($"/api/manage/bingnews?id={id}");
+
+			return Json(re);
+		}
 	}
 }
