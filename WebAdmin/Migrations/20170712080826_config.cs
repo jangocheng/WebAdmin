d87b0a850a40fdcc4bd6b21e3e08d300c810429c
NilTor <chenxuesong@cisscool.com>
2017-07-12 16:41:41 +0800

    db update

---
+++ WebAdmin/Migrations/20170712080826_config.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebAdmin.Migrations
+{
+    public partial class config : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Config",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 32, nullable: true),
+                    Type = table.Column<string>(maxLength: 32, nullable: true),
+                    Value = table.Column<string>(maxLength: 4000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Config", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Config");
+
+        }
+    }
+}
