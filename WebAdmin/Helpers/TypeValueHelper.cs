51aed7d5ef54db3230f86f419dfc6d207c8f9758
NilTor <chenxuesong@cisscool.com>
2017-07-24 17:39:25 +0800

    improve page

---
+++ WebAdmin/Helpers/TypeValueHelper.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdmin.Helpers
+{
+    public class TypeValueHelper
+    {
+        public static string[] Language = { "中文", "英文" };
+        public static string[] ResourceType = { "文档", "视频", "软件", "代码" ,"链接"};
+
+    }
+}
