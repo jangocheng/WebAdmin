8bc62dd88ea1353a574297d2f4e6f515586f2b67
NilTor <chenxuesong@cisscool.com>
2017-07-24 16:44:43 +0800

    + Catalog ,Edit page

--- WebAdmin/Controllers/ConfigController.cs
+++ WebAdmin/Controllers/ConfigController.cs
@@ -7,112 +7,144 @@ using AutoMapper;
 using MSDev.DB;
 using WebAdmin.FormModels.Config;
 using static System.String;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebAdmin.Controllers
 {
     public class ConfigController : BaseController
-	{
-		private readonly AppDbContext _context;
-		private readonly IMapper _mapper;
-		public ConfigController(IMapper mapper, AppDbContext context)
-		{
-			_mapper = mapper;
-			_context = context;
-		}
-		[HttpGet]
-		public IActionResult Index()
-		{
-			return View();
-		}
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+        public ConfigController(IMapper mapper, AppDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-		[HttpGet]
-		public IActionResult Catalog()
-		{
-			//获取所有目录
-			var catalogs = _context.Catalog.ToList();
-			ViewBag.Catalogs = catalogs;
-			return View();
-		}
+        [HttpGet]
+        public IActionResult Catalog()
+        {
+            //获取所有目录
+            var catalogs = _context.Catalog.ToList();
+            ViewBag.Catalogs = catalogs;
+            return View();
+        }
 
-		/// <summary>
-		/// 添加目录
-		/// </summary>
-		/// <param name="catalogForm"></param>
-		/// <returns></returns>
-		[HttpPost]
-		public IActionResult AddCatalog(CatalogForm catalogForm)
-		{
-			if (ModelState.IsValid)
-			{
-				Catalog topCatalog = null;
-				if (IsNullOrEmpty(catalogForm.TopCatalog))
-				{
-					catalogForm.IsTop = 1;
-				}
-				else
-				{
-					catalogForm.IsTop = 0;
-					topCatalog = _context.Catalog.Find(Guid.Parse(catalogForm.TopCatalog));
-				}
+        [HttpGet]
+        public IActionResult EditCatalog(string id)
+        {
+            if (IsNullOrEmpty(id))
+            {
+                RedirectToAction("Catalog");
+            }
+            var catalog = _context.Catalog
+                .Include(m => m.TopCatalog)
+                .SingleOrDefault(m => m.Id == Guid.Parse(id));
 
-				var catalog = new Catalog()
-				{
-					Id = Guid.NewGuid(),
-					CreatedTime = DateTime.Now,
-					UpdatedTime = DateTime.Now,
-					IsTop = catalogForm.IsTop,
-					Name = catalogForm.Name,
-					Status = 0,
-					TopCatalog = topCatalog,
-					Type = catalogForm.Type,
-					Value = catalogForm.Value
-				};
+            return View(catalog);
+        }
 
-				_context.Catalog.Add(catalog);
-				_context.SaveChanges();
-			}
-			return RedirectToAction("Catalog", catalogForm);
-		}
+        [HttpPost]
+        public IActionResult UpdateCatalog(Catalog catalog)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_context.Catalog.Any(m => m.Id == catalog.Id))
+                {
+                    catalog.UpdatedTime = DateTime.Now;
+                    _context.Update(catalog);
+                    _context.SaveChanges();
 
-		[HttpGet]
-		public IActionResult Config()
-		{
-			ViewBag.Configs = _context.Config.ToList();
-			return View();
-		}
+                   return RedirectToAction("Catalog");
+                }
+            }
+            return View("EditCatalog", catalog);
+        }
+
+        /// <summary>
+        /// 添加目录
+        /// </summary>
+        /// <param name="catalogForm"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult AddCatalog(CatalogForm catalogForm)
+        {
+            if (ModelState.IsValid)
+            {
+                Catalog topCatalog = null;
+                if (IsNullOrEmpty(catalogForm.TopCatalog))
+                {
+                    catalogForm.IsTop = 1;
+                }
+                else
+                {
+                    catalogForm.IsTop = 0;
+                    topCatalog = _context.Catalog.Find(Guid.Parse(catalogForm.TopCatalog));
+                }
+
+                var catalog = new Catalog()
+                {
+                    Id = Guid.NewGuid(),
+                    CreatedTime = DateTime.Now,
+                    UpdatedTime = DateTime.Now,
+                    IsTop = catalogForm.IsTop,
+                    Name = catalogForm.Name,
+                    Status = 0,
+                    TopCatalog = topCatalog,
+                    Type = catalogForm.Type,
+                    Value = catalogForm.Value
+                };
+
+                _context.Catalog.Add(catalog);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Catalog", catalogForm);
+        }
+
+        [HttpGet]
+        public IActionResult Config()
+        {
+            ViewBag.Configs = _context.Config.ToList();
+            return View();
+        }
 
 
-		[HttpPost]
-		public IActionResult AddConfig(ConfigForm configForm)
-		{
-			if (ModelState.IsValid)
-			{
-				var config = new Config()
-				{
-					Name = configForm.Name,
-					Type = configForm.Type,
-					Value = configForm.Value,
-					Id = Guid.NewGuid()
-				};
+        [HttpPost]
+        public IActionResult AddConfig(ConfigForm configForm)
+        {
+            if (ModelState.IsValid)
+            {
+                var config = new Config()
+                {
+                    Name = configForm.Name,
+                    Type = configForm.Type,
+                    Value = configForm.Value,
+                    Id = Guid.NewGuid()
+                };
 
-				_context.Config.Add(config);
-				_context.SaveChanges();
+                _context.Config.Add(config);
+                _context.SaveChanges();
 
-			}
-			return RedirectToAction("Config", configForm);
-		}
+            }
+            return RedirectToAction("Config", configForm);
+        }
 
-		/// <summary>
-		/// 删除一条配置
-		/// </summary>
-		/// <returns></returns>
-		public IActionResult DelConfig([FromRoute]string id)
-		{
-			var config = _context.Config.Find(Guid.Parse(id));
-			_context.Config.Remove(config);
-			_context.SaveChanges();
-			return RedirectToAction("Config");
-		}
-	}
+        /// <summary>
+        /// 删除一条配置
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult DelConfig([FromRoute]string id)
+        {
+            var config = _context.Config.Find(Guid.Parse(id));
+            _context.Config.Remove(config);
+            _context.SaveChanges();
+            return RedirectToAction("Config");
+        }
+    }
 
 }
