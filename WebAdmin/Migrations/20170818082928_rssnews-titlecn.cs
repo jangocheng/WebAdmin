f460f722363ad8a95844a5262d777fdf98634dad
NilTor <chenxuesong@cisscool.com>
2017-08-18 16:30:10 +0800

    update models

---
+++ WebAdmin/Migrations/20170818082928_rssnews-titlecn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebAdmin.Migrations
+{
+    public partial class rssnewstitlecn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TitleCn",
+                table: "RssNews",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TitleCn",
+                table: "RssNews");
+        }
+    }
+}
