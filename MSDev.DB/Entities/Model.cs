52b992ed9cf8563ff7607034ddb0507330ebde66
NilTor <zpty@outlook.com>
2018-01-17 19:06:26 +0800

    add  model

--- MSDev.DB/Entities/Model.cs
+++ MSDev.DB/Entities/Model.cs
@@ -8,20 +8,23 @@ namespace MSDev.DB.Entities
         /// <summary>
         /// Guid
         /// </summary>
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
         /// <summary>
         /// 状态值
         /// </summary>
-        public int? Status { get; set; }
+        public int? Status { get; set; } = 0;
 
 
         /// <summary>
         /// 创建时间
         /// </summary>
-        public DateTime CreatedTime { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
         /// <summary>
         /// 更新时间
         /// </summary>
-        public DateTime UpdatedTime { get; set; }
+        public DateTime UpdatedTime { get; set; } = DateTime.Now;
+
+
+        //public bool IsDelete { get; set; }
     }
 }
